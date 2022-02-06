289f5cdcdd7d92c52c2371494070a6880c71bed0
AndreyCas91 <VfcnMGbrF11!>
2022-02-06 16:52:47 +0700

    Домашнее задание по 3 уроку

--- app/src/main/java/com/gb/libraries/GithubUsersRepo.kt
+++ app/src/main/java/com/gb/libraries/GithubUsersRepo.kt
@@ -1,10 +1,16 @@
 package com.gb.libraries
 
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.subjects.BehaviorSubject
+
 class GithubUsersRepo {
     private val repositories =
         (0..100).map { GithubUser("login $it") }
 
-    fun getUsers(): List<GithubUser> {
-        return repositories
+    private val bs = BehaviorSubject.fromIterable(listOf(repositories))
+
+    fun getUsers(): Observable<List<GithubUser>>? {
+        return bs
     }
+
 }
--- app/src/test/java/com/gb/libraries/ExampleUnitTest.kt
+++ app/src/test/java/com/gb/libraries/ExampleUnitTest.kt
@@ -1,7 +1,9 @@
 package com.gb.libraries
 
-import org.junit.Assert.assertEquals
+import io.reactivex.rxjava3.core.Observable
 import org.junit.Test
+import java.util.concurrent.TimeUnit
+
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,6 +13,34 @@ import org.junit.Test
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        println("проверка")
+        val obs = Observable.just(3, 6)
+            .switchMap { count ->
+                Observable.interval(100, TimeUnit.MILLISECONDS)
+                    .map { "из события номер $count счетчик $it" }
+                    .take(count.toLong())
+            }
+
+        /*
+        * switchMap предоставляет нам элементы последнего события, пригодится когда промежуточные данные не нужны.
+        * */
+
+        obs
+            .map { it }
+            .doOnSubscribe { println("Начало выполнения") }
+            .subscribe({
+                println("пришли данные = $it")
+            }, {
+                    println("error")
+               }, {
+                println("поток завершился") }
+            )
+
+        Thread.sleep(100000)
     }
+
+
 }
+
+
+
